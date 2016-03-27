a0a4917eaa4e0b121d6a915410ef0400786af1fb
songzy12 <644879054@qq.com>
2016-03-27 21:24:12 +0800

    Offer 3

---
+++ 比赛/[Offer收割]编程练习赛3/D.子矩阵求和.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<list>
+#include<map>
+#include<vector>
+#include<cstdio>
+#include<climits>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);
+	
+    int Q;
+    cin>>Q;
+    while (Q--) {
+        int N, M, K;
+        cin>>N>>M>>K;
+        
+        
+    }
+	return 0;
+}
