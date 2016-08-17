8051d75e627fb7a1e88786eac99b878a81bfec10
Andy <songzy.thu@qq.com>
2016-08-17 12:10:02 +0800

    give me test back

---
+++ hiho一下/101-110/110.Increase Charisma Points.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <vector>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	
+	return 0;
+}
