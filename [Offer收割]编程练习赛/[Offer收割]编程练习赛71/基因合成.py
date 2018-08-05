a047d1a138d205cf86b918585bb2811ec0b5b76f
songzy <songzy.thu@qq.com>
2018-08-06 00:23:07 +0800

    Mon Aug  6 00:23:07 CST 2018

--- [Offer收割]编程练习赛/[Offer收割]编程练习赛71/基因合成.py
+++ [Offer收割]编程练习赛/[Offer收割]编程练习赛71/基因合成.py
@@ -1,5 +1,6 @@
 T = int(raw_input())
 
+
 def valid(S, i, j):
     if (j - i) % 2 == 0:
         return False
@@ -11,12 +12,13 @@ def valid(S, i, j):
         j -= 1
     return True
 
+
 def get_dp(S, i, j, dp):
     if i == j:
         return 1
     if (i, j) in dp:
         return dp[i, j]
-    
+
     ans = 1 + min(get_dp(S, i + 1, j, dp), get_dp(S, i, j - 1, dp))
     if valid(S, i, j):
         ans = min(ans, 1 + get_dp(S, i, (i + j) // 2, dp))
@@ -25,12 +27,15 @@ def get_dp(S, i, j, dp):
 
 
 def solve(S):
-    dp = {}
     if not S:
         return 0
-    return get_dp(S, 0, len(S) - 1, dp)
+    return get_dp(S, 0, len(S) - 1, {})
 
 
 for t in range(T):
     S = raw_input()
     print(solve(S))
+
+# https://difusal.blogspot.com/2014/08/manachers-algorithm-longest-palindromic.html
+# https://www.geeksforgeeks.org/palindromic-tree-introduction-implementation/
+# https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
